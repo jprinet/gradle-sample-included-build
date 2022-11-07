4f85cf731e76aa533e30276ffe276c27015507fd
Jerome Prinet <jprinet@gradle.com>
2022-11-07 17:29:38 +0100

    Add build cache

--- gradle-plugins/src/main/groovy/com/sample/plugins/SamplePlugin.groovy
+++ gradle-plugins/src/main/groovy/com/sample/plugins/SamplePlugin.groovy
@@ -9,16 +9,18 @@ import com.gradle.enterprise.gradleplugin.GradleEnterprisePlugin;
 import com.gradle.scan.plugin.BuildScanExtension;
 import com.gradle.scan.plugin.BuildScanPlugin;
 import com.gradle.CommonCustomUserDataGradlePlugin
+import org.gradle.caching.configuration.BuildCacheConfiguration
+import org.gradle.caching.http.HttpBuildCache
 
 class SamplePlugin implements Plugin<Settings> {
 
   @Override
   void apply(Settings settings) {
     settings.getPluginManager().apply(io.invertase.gradle.settings.SettingsPlugin.class)
-//    settings.getPluginManager().apply(com.github.jengelman.gradle.plugins.shadow.PluginShadowPlugin.class)
     settings.getPluginManager().apply(CommonCustomUserDataGradlePlugin.class);
     settings.getPluginManager().apply(GradleEnterprisePlugin.class);
     configureGradleEnterprise(settings.getExtensions().getByType(GradleEnterpriseExtension.class))
+    configureBuildCache(settings.getBuildCache())
   }
 
   private void configureGradleEnterprise(GradleEnterpriseExtension gradleEnterprise) {
@@ -26,5 +28,18 @@ class SamplePlugin implements Plugin<Settings> {
     BuildScanExtension buildScan = gradleEnterprise.getBuildScan();
     buildScan.publishAlways();
     buildScan.capture.taskInputFiles = true
+  }
+
+  private void configureBuildCache(BuildCacheConfiguration buildCache) {
+    buildCache.local(local -> {
+      local.setEnabled(false);
+    });
+    buildCache.remote(HttpBuildCache.class, remote -> {
+      remote.setUrl("http://localhost:5086/cache/")
+      remote.setAllowUntrustedServer(true)
+      remote.allowInsecureProtocol(true)
+      remote.setEnabled(true)
+      remote.setPush(true)
+    });
   }
 }
